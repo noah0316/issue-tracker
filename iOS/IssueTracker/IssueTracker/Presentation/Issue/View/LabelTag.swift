c9839b7535a448f27d4ea4f7acc5f6d8bde21371
demiiann <tkim0156@gmail.com>
2023-05-23 21:40:02 +0900

    FEAT: LabelTag 인터페이스 작성 (#80)
    - 배경색을 매개변수로 해당 색상코드에 맞는 배경색을 지정함
    - 배경색의 종류에 따라 LabelTag textColor가 흰색 또는 검은색으로 변경됨

--- iOS/IssueTracker/IssueTracker/Presentation/Issue/View/LabelTag.swift
+++ iOS/IssueTracker/IssueTracker/Presentation/Issue/View/LabelTag.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class BadgeLabel: UILabel {
+final class LabelTag: UILabel {
     private let edgeInsets = UIEdgeInsets(top: 4.0, left: 16.0, bottom: 4.0, right: 16.0)
     
     override var intrinsicContentSize: CGSize {
@@ -21,8 +21,8 @@ final class BadgeLabel: UILabel {
         self.init()
         self.text = name
         self.font = FontStyle.label
-        self.textColor = .white
-        self.setBackgroundColor()
+        self.setLabelTagBackgroundColor("#c2b9a9")
+        self.setLabelTagTextColor()
         self.sizeToFit()
     }
     
@@ -36,7 +36,33 @@ final class BadgeLabel: UILabel {
         self.layer.masksToBounds = true
     }
     
-    func setBackgroundColor() {
-        self.backgroundColor = ColorStyle.issueTrackerBlue
+    func setLabelTagBackgroundColor(_ backgroundColor: String) {
+        self.backgroundColor = UIColor().setBackgroundColorFromHex(backgroundColor)
+    }
+    
+    func setLabelTagTextColor() {
+        let rgb = self.backgroundColor?.cgColor.components?.dropLast() ?? [0, 0, 0]
+        let multipliedRgb = rgb.map { $0 * 255 }
+        if (multipliedRgb[0] * 0.299 + multipliedRgb[1] * 0.587 + multipliedRgb[2] * 0.114) > 186 {
+            self.textColor = .black
+        } else { self.textColor = .white }
+    }
+}
+
+extension UIColor {
+    func setBackgroundColorFromHex(_ hex: String) -> UIColor? {
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if hexString.hasPrefix("#") {
+            hexString.remove(at: hexString.startIndex)
+        }
+        
+        var rgb: UInt32 = 0
+        Scanner(string: hexString).scanHexInt32(&rgb)
+        
+        return UIColor(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+                       green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+                       blue: CGFloat(rgb & 0x0000FF) / 255.0,
+                       alpha: 1.0)
     }
 }
