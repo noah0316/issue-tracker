f1d9cc7f1cc01f16e8e8d7f269f9d49e813981c1
demiiann <tkim0156@gmail.com>
2023-05-23 13:27:53 +0900

    ADD: LabelTag 파일 프로젝트 경로에 추가 및 코드 수정 (#63)
    - 컨벤션을 위반한 코드 부분 수정

--- iOS/IssueTracker/IssueTracker/Presentation/Issue/View/LabelTag.swift
+++ iOS/IssueTracker/IssueTracker/Presentation/Issue/View/LabelTag.swift
@@ -7,31 +7,33 @@
 
 import UIKit
 
-class LabelTag: UILabel {
-    private static let padding = UIEdgeInsets(top: 4.0, left: 16.0, bottom: 4.0, right: 16.0)
+final class BadgeLabel: UILabel {
+    private let edgeInsets = UIEdgeInsets(top: 4.0, left: 16.0, bottom: 4.0, right: 16.0)
+    
     override var intrinsicContentSize: CGSize {
         var contentSize = super.intrinsicContentSize
-        contentSize.width += Self.padding.left + Self.padding.right
-        contentSize.height += Self.padding.top + Self.padding.bottom
+        contentSize.width += edgeInsets.left + edgeInsets.right
+        contentSize.height += edgeInsets.top + edgeInsets.bottom
         return contentSize
     }
     
-    func configure(name: String) {
-        text = name
-        font = UIFont.systemFont(ofSize: 12, weight: .bold, width: .standard)
-        textColor = .white
-        setBackgroundColor()
+    convenience init(name: String) {
+        self.init()
+        self.text = name
+        self.font = FontStyle.label
+        self.textColor = .white
+        self.setBackgroundColor()
         self.sizeToFit()
     }
     
     override func drawText(in rect: CGRect) {
-        super.drawText(in: rect.inset(by: Self.padding))
+        super.drawText(in: rect.inset(by: self.edgeInsets))
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        layer.cornerRadius = ComponentStyle.Radius.regular
-        layer.masksToBounds = true
+        self.layer.cornerRadius = ComponentStyle.Radius.regular
+        self.layer.masksToBounds = true
     }
     
     func setBackgroundColor() {
