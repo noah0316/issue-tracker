d1d353eef99e068e2ef686f711f907af1dee3bfc
birdieHyun <flaehdan@gmail.com>
2023-05-31 10:14:47 +0900

    ADD: 회원가입 시 중복 회원 체크(#116)

--- BE/src/main/java/issuetracker/issuetracker/domain/user/MemberRepository.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/user/MemberRepository.java
@@ -1,16 +1,14 @@
 package issuetracker.issuetracker.domain.user;
 
-import issuetracker.issuetracker.domain.issue.Assignee;
-import issuetracker.issuetracker.domain.user.dto.AssigneeDTO;
 import issuetracker.issuetracker.domain.user.dto.AuthorDTO;
-import issuetracker.issuetracker.domain.user.dto.AuthorFilterDTO;
-import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import issuetracker.issuetracker.domain.user.login.dto.UserProfileResponse;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
@@ -27,6 +25,9 @@ public interface MemberRepository extends CrudRepository<Member, Long> {
 
     @Query("SELECT member.member_id AS id, member.member_name AS name," +
             " member.profile_url AS profile_url FROM member WHERE member.member_id = :userId")
-    AuthorDTO findByAuthorDTO(@Param("userId") Long userId);
+    Optional<AuthorDTO> findByAuthorDTO(@Param("userId") Long userId);
 
+//    @Modifying
+    @Query("INSERT INTO member (member_id, member_name, profile_url) VALUES (:#{#loginDTO.memberId}, :#{#loginDTO.memberName}, :#{#loginDTO.profileUrl})")
+    void joinMember(@Param("loginDTO") UserProfileResponse loginDTO);
 }
