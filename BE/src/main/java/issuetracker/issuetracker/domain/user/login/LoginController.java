3de91484b7310f23206c258d33b423c1af4fd61e
birdieHyun <flaehdan@gmail.com>
2023-05-31 17:40:41 +0900

    ADD: JWT 토큰을 response body로 전송  (#116)

--- BE/src/main/java/issuetracker/issuetracker/domain/user/login/LoginController.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/user/login/LoginController.java
@@ -2,11 +2,10 @@ package issuetracker.issuetracker.domain.user.login;
 
 import issuetracker.issuetracker.domain.user.MemberService;
 import issuetracker.issuetracker.domain.user.login.dto.GithubToken;
+import issuetracker.issuetracker.domain.user.login.dto.JWTResponse;
 import issuetracker.issuetracker.domain.user.login.dto.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +22,7 @@ public class LoginController {
     private final MemberService service;
 
     @GetMapping("/githublogin")
-    public ResponseEntity<String> githubLogin(String code, HttpServletResponse response) {
+    public ResponseEntity<JWTResponse> githubLogin(String code, HttpServletResponse response) {
         GithubToken githubToken = loginService.getAccessToken(code);
         response.setHeader("Authorization", "application/json");
 
@@ -32,17 +31,7 @@ public class LoginController {
 
         String token = jwtUtil.createToken(userProfile);
 
-        HttpHeaders header = makeTokenHeader(token);
-
-        return ResponseEntity.ok()
-                .headers(header)
-                .body("login success");
+        return ResponseEntity.ok(new JWTResponse("login success", token));
     }
 
-    private HttpHeaders makeTokenHeader(String token) {
-        HttpHeaders header = new HttpHeaders();
-        header.set("Authorization", "Bearer " + token);
-        header.setContentType(MediaType.APPLICATION_JSON);
-        return header;
-    }
 }
