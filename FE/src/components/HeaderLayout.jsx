9ef171e86c9913a0f92a33f40bc66891edd1b2f3
yesolyo <ahnyelim77@gmail.com>
2023-06-01 11:47:23 +0900

    FEAT: 로그인 제외 페이지 헤더 생성 (#149)
    - 로그인 페이지에서는 헤더가 보이지 않게 구현하였습니다.
    - 로그인 정보를 props로 전달하였습니다.

---
+++ FE/src/components/HeaderLayout.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { Header } from './Header';
+import { parseJwt } from '../utils/parseJwt';
+
+export const HeaderLayout = () => {
+  const [user, setUser] = useState(null);
+
+  const initData = async () => {
+    const userData = parseJwt(localStorage.jwtToken);
+    setUser(userData);
+  };
+
+  useEffect(() => {
+    initData();
+  }, []);
+
+  return (
+    <>
+      <Header userData={user} />
+      <DefaultLayout>
+        <Outlet context={{ user }} />
+      </DefaultLayout>
+    </>
+  );
+};
+
+const DefaultLayout = styled.div`
+  width: 100%;
+`;
