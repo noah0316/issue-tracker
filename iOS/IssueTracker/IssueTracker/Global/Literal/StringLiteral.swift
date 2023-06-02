9efa5c10357fb857524eed5fd492ba80cd18e6d0
noah0316 <dev.noah0316@gmail.com>
2023-06-02 13:48:51 +0900

    CHORE: 에러 메시지 추가 (#134)
    - 네트워크 에러 메시지를 표시하기 위한 StringLiteral을 추가하였습니다.

--- iOS/IssueTracker/IssueTracker/Global/Literal/StringLiteral.swift
+++ iOS/IssueTracker/IssueTracker/Global/Literal/StringLiteral.swift
@@ -11,4 +11,18 @@ enum StringLiteral {
     enum IssueListViewController {
         static let title = "이슈"
     }
+    
+    enum NetworkServiceError {
+        static let badRequest = "잘못된 요청입니다."
+        static let fobidden = "권한이 없습니다."
+        static let notFound = "콘텐츠를 찾을 수 없습니다."
+        static let internalServerError = "서버 에러"
+        static let serviceUnAvailable = "서비스를 사용할 수 없습니다."
+        static let unknownError = "알 수 없는 에러"
+        static let urlError = "잘못된 URL입니다."
+    }
+    
+    enum ErrorAlertController {
+        static let title = "네트워크 에러"
+    }
 }
