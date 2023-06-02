baf4c5db44c39b7b5a1969a5fc78d4dfb090188f
birdieHyun <flaehdan@gmail.com>
2023-06-02 10:11:42 +0900

    ADD: file upload

--- BE/src/main/java/issuetracker/issuetracker/infrastructure/aws/FileUploadController.java
+++ BE/src/main/java/issuetracker/issuetracker/infrastructure/aws/FileUploadController.java
@@ -4,7 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 @RestController
 public class FileUploadController {
@@ -16,11 +15,11 @@ public class FileUploadController {
     }
 
     @PostMapping("/upload")
-    public PutObjectResponse uploadFile(@RequestParam("file") MultipartFile file) {
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
         String url = "https://issue-tracker-team04.s3.ap-northeast-2.amazonaws.com/profile/" +file.getOriginalFilename();
-        System.out.println(url);
 
-        return fileUploadService.uploadFile(file);
+        fileUploadService.uploadFile(file);
+        return url;
     }
 }
 
