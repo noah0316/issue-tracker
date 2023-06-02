6ea2786e376e5f7c72bc98fcfdb3bf980668d0ae
noah0316 <dev.noah0316@gmail.com>
2023-06-02 13:44:13 +0900

    ADD: NetworkRequestable 프로토콜 추가 (#134)
    - 의존성 역전을 위한 NetworkRequestable 프로토콜을 추가하였습니다.

---
+++ iOS/IssueTracker/IssueTracker/Network/Protocol/NetworkRequestable.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkRequestable.swift
+//  IssueTracker
+//
+//  Created by Noah on 2023/05/31.
+//
+
+import Combine
+import Foundation
+
+protocol NetworkRequestable {
+    func requestData(from networkRequest: NetworkRequest) -> AnyPublisher<Data, Error>
+}
