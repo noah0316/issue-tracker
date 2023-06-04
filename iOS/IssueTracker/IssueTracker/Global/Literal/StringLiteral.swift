2d3fdae3211ce36035bca3b2c843efa310fb8df9
noah0316 <dev.noah0316@gmail.com>
2023-06-04 13:55:51 +0900

    ADD: Error StringLiteral 추가 (#134)
    - 에러에 대한 localizedString에서 사용할 StringLiteral을 추가하였습니다.

--- iOS/IssueTracker/IssueTracker/Global/Literal/StringLiteral.swift
+++ iOS/IssueTracker/IssueTracker/Global/Literal/StringLiteral.swift
@@ -8,21 +8,32 @@
 import Foundation
 
 enum StringLiteral {
+    static let emptyString = ""
+    
     enum IssueListViewController {
         static let title = "이슈"
     }
     
-    enum NetworkServiceError {
+    enum HTTPResponseError {
         static let badRequest = "잘못된 요청입니다."
         static let fobidden = "권한이 없습니다."
         static let notFound = "콘텐츠를 찾을 수 없습니다."
         static let internalServerError = "서버 에러"
         static let serviceUnAvailable = "서비스를 사용할 수 없습니다."
         static let unknownError = "알 수 없는 에러"
-        static let urlError = "잘못된 URL입니다."
+        static let badURLError = "잘못된 URL입니다."
+    }
+    
+    enum IssueRepositoryError {
+        static let decoding = "요청에 대한 응답을 분석할 수 없습니다."
+    }
+    
+    enum IssueUseCaseError {
+        static let unknown = "알 수 없는 에러입니다."
     }
     
     enum ErrorAlertController {
         static let title = "네트워크 에러"
+        static let confirm = "확인"
     }
 }
