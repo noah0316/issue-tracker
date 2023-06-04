a3dcc85058f3e6e03565a10d70a3960f7478e95d
noah0316 <dev.noah0316@gmail.com>
2023-06-04 13:18:57 +0900

    CHORE: NetworkSerivice Error 처리 (#134)
    - NetworkService에서 발생하는 에러를 NetworkServiceError 타입으로 변환하여 throw하도록 처리하였습니다.

--- iOS/IssueTracker/IssueTracker/Network/Common/NetworkService.swift
+++ iOS/IssueTracker/IssueTracker/Network/Common/NetworkService.swift
@@ -15,16 +15,17 @@ struct NetworkService: NetworkRequestable {
         self.session = session
     }
     
+    // swiftlint:disable function_body_length
     /// Web Service의 end point 호출을 할 때 사용합니다.
     /// - note: 공식문서에서는 remote server 와의 소규모 인터렉션(Web service endpoint 호출과 같은)용도에 URLSessionDataTask를 사용하는 것이 이상적이라 하여
     /// 이를 근거로 URLSessionDataTask를 사용하였습니다.
     ///
     /// - seealso: [Apple Developer Documentation: URL Loading System](https://developer.apple.com/documentation/foundation/url_loading_system/fetching_website_data_into_memory#overview)
-    func requestData(from networkRequest: NetworkRequest) -> AnyPublisher<Data, Error> {
+    func requestData(from networkRequest: NetworkRequest) -> AnyPublisher<Data, NetworkServiceError> {
         return self.session.dataTaskPublisher(for: networkRequest.buildURLRequest())
             .tryMap { (data: Data, response: URLResponse) in
                 guard let httpResponseStatus = HTTPResponseStatus(urlResponse: response)
-                else { throw URLError(.badServerResponse) }
+                else { throw NetworkServiceError.urlError(error: .init(.badServerResponse)) }
                 
                 if httpResponseStatus.isSuccessful() {
                     return data
@@ -32,6 +33,22 @@ struct NetworkService: NetworkRequestable {
                 
                 throw httpResponseStatus.makeNetworkServiceError()
             }
+            .mapError { error -> NetworkServiceError in
+                return self.convertErrorToNetworkServiceError(with: error)
+            }
             .eraseToAnyPublisher()
+    }
+    // swiftlint:enable function_body_length
+}
+
+extension NetworkService {
+    private func convertErrorToNetworkServiceError(with error: Error) -> NetworkServiceError {
+        if let networkError = error as? NetworkServiceError {
+            return networkError
+        } else if let urlError = error as? URLError {
+            return NetworkServiceError.urlError(error: urlError)
+        } else {
+            return NetworkServiceError.httpResponseError(error: .unknownError)
+        }
     }
 }
